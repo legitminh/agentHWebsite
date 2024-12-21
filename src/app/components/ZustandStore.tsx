16ef583c74e1b9875efe730909787afe40bf0dad
legitMinh <90986973+legitminh@users.noreply.github.com>
2024-12-20 21:09:47 -0500

    Fix: State management persist through refresh
    Used Zustand.middleware

--- src/app/components/ZustandStore.tsx
+++ src/app/components/ZustandStore.tsx
@@ -1,13 +1,22 @@
-import {create } from 'zustand'
+import {create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
 interface GenerationState {
     theme: string;
     audioURL: string; 
     setTheme: (theme: string) => void;
     setAudioURL: (audioURL: string) => void; 
 }
-export const useGenerationStore = create<GenerationState>()((set) => ({
-    theme: "dark",
-    audioURL: "",
-    setTheme: (theme: string) => set({theme}),
-    setAudioURL: (audioURL: string) => set({ audioURL })
-}))
+export const useGenerationStore = create<GenerationState>()(
+    persist(
+        (set) => ({
+            theme: "dark",
+            audioURL: "",
+            setTheme: (theme: string) => set({ theme }),
+            setAudioURL: (audioURL: string) => set({ audioURL }),
+        }),
+        {
+            name: 'generation-store', // key used in localStorage
+        }
+    )
+)
